e8e0cd544064b6335b3c03bc8d2075829eb62a3a
Guilherme José Afonso <guilermejs77@gmail.com>
2026-04-23 15:36:43 -0300

    Add files via upload

---
+++ Gerenciador_veiculos2.0/SistemVehicle/Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemVehicle.Models
+{
+    internal class Usuario
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public int Id { get; private set; }
+        public string Tipo { get; private set; }
+
+        public Usuario() { }
+        public Usuario(string email, string senha) { }
+
+        public Usuario(string nome, DateTime dataNascimento, string telefone, string email, string senha)
+        {
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            Telefone = telefone;
+            Email = email;
+            Senha = senha;
+        }
+
+    }
+}
